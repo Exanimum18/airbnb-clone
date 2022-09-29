e5ef2da122cbe65c583e77584c35ebd377549bba
Santiago Baron Castaño <santiago.baron.18@gmail.com>
2022-09-29 15:51:09 -0500

    Se agregaron atributos a la tabla de plans

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_29_000412) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_29_204303) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_29_000412) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "location"
+    t.string "category"
+    t.string "intensity"
+    t.datetime "starting_time"
+    t.datetime "finish_time"
     t.index ["user_id"], name: "index_plans_on_user_id"
   end
 
