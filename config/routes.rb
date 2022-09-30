7f1738b475ed86325c437dfff9e1141cea5cd27d
michael espinal <michaelec519@gmail.com>
2022-09-30 14:45:00 -0500

    solucion conflicto root

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
-  # do
-  #   resources :plans
-  #   resources :bookings
-  # end
+  root to: "plans#index"
 
+  resources :plans do
+    resources :bookings
+  end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
