e394f3b510b45f9dfafc8f7c187940db5d5db029
Santiago Baron Castaño <santiago.baron.18@gmail.com>
2022-10-04 19:09:27 -0500

    Creadon los bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,21 +5,36 @@ class BookingsController < ApplicationController
   end
 
   def show
-
   end
 
   def new
+    @plan = Plan.find(params[:plan_id])
+    @booking = Booking.new
   end
 
   def create
-  end
+    #localhost:3000/plans/4/bookings/new
+    @plan = Plan.find(params[:plan_id])
+    #@plan => id+4 categroty+"¨sdfsfs",
+    @booking = Booking.new(booking_params)
+    #@booking.id = nil
+    #@booking.user_id = nil
+    #@booking.plan_id = nil
+    #@booking.status = "¨por confirmar"
+    @booking.user_id = current_user.id
+    #@booking.user_id = 3
+    @booking.plan_id = @plan.id
 
-  def destroy
+    if @booking.save
+      redirect_to plan_bookings_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
-  def set_booking
-    @booking = Booking.find(params[:plan_id])
+  def booking_params
+    params.require(:booking).permit(:status)
   end
 end
