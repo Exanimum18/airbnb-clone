a97548cdde4199c0a8baebe08ef246d317547e24
Alejandra Guarin <alejaguarin@gmail.com>
2022-10-07 12:18:50 -0500

    cloud

--- db/schema.rb
+++ db/schema.rb
@@ -43,13 +43,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_07_162553) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "plan_id", null: false
+    t.integer "plan_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "status"
-    t.index ["plan_id"], name: "index_bookings_on_plan_id"
-    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "plans", force: :cascade do |t|
@@ -83,7 +81,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_07_162553) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "bookings", "plans"
-  add_foreign_key "bookings", "users"
   add_foreign_key "plans", "users"
 end
